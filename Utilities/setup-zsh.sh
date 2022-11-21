60132c23c7c3577826bd80f794f96261ae43bea4
atulsingh0 <victimizeit@gmail.com>
2022-11-21 15:33:22 -0500

    master | added plugins

--- Utilities/setup-zsh.sh
+++ Utilities/setup-zsh.sh
@@ -17,3 +17,28 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 # Insllating powerlevel10k theme
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >>~/.zshrc
+
+# Installing Plugins
+
+## zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+## zsh-history-substring-search
+git clone https://github.com/zsh-users/zsh-history-substring-search ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+
+## zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+
+## zsh-completions
+git clone https://github.com/zsh-users/zsh-completions ${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions
+
+##  history-search-multi-word
+git clone https://github.com/z-shell/H-S-MW.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/history-search-multi-word
+
+# Update Plugin
+grep ^plugins ~/.zshrc |
+    sed 's_)_zsh-syntax-highlighting zsh-history-substring-search zsh-autosuggestions history-search-multi-word zsh-completions history-search-multi-word _' |
+    cut -d'(' -f2 | tr ' ' '\n' | sort -u | tr '\n' ' '
+
+# Open ~/.zshrc and replace the content of "plugins" from above command output
+plugins=(... zsh-syntax-highlighting zsh-history-substring-search zsh-autosuggestions history-search-multi-word zsh-completions history-search-multi-word)
