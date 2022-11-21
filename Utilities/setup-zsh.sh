294d3d86a4b5a9c7d9b3142655f98f195467a056
atulsingh0 <victimizeit@gmail.com>
2022-11-21 14:10:52 -0500

    master | added powerlevel theme

--- Utilities/setup-zsh.sh
+++ Utilities/setup-zsh.sh
@@ -1,14 +1,19 @@
 # Considering the base OS as ubuntu
-
-# change the installer based on your OS
-inst="apt"
+# You have to use appropriate installed command (yum, dnf etc) for your OS
 
 # Installing ZSH
-sudo $inst update
-sudo $inst install zsh -y
+sudo apt update
+sudo apt install zsh -y
 
 # Switch the default Shell
 sudp chsh -s $(which zsh)
 
 # To check if you are now using ZSH
 echo $SHELL
+
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+# Insllating powerlevel10k theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
+echo 'ZSH_THEME="powerlevel10k/powerlevel10k"' >>~/.zshrc
